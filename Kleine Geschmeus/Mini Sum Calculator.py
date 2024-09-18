d061b06aee45aa340a11c8eda62f4eb46fc7376c
Kieran Rae <stolar.viski@gmail.com>
2024-09-18 13:23:26 +0200

    Add files via upload

---
+++ Kleine Geschmeus/Mini Sum Calculator.py
@@ -0,0 +1,9 @@
+number_1 = input ("First Number ")
+int(number_1)
+number_2 = input ("Second Number ")
+int(number_2)
+if int(number_1) + int(number_2) == 69:
+    print ("nice")
+else :
+    sum = int(number_1) + int(number_2)
+    print(sum)

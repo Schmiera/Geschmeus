520722fa60d2f5aac08d1ef09ae67c87786859e0
Kieran Rae <stolar.viski@gmail.com>
2024-09-18 13:06:35 +0200

    Delete Greeter Bot.py

--- Greeter Bot.py
+++
@@ -1,5 +0,0 @@
-name = input("Was isch din Name? ")
-if name == ("Sven") or ("sven"):
-    print ("Woah, de name tönt echt dumm!!! Haha ich bin froh heiss ich nöd so lol. " + name + " more like Svengay!!!")
-else: print (name + " isch aber en schöne name")
-

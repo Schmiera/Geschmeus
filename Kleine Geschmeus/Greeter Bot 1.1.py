d1a93e1d8224688b2d18fb910e2dba6897bd9b30
Kieran Rae <stolar.viski@gmail.com>
2024-09-19 08:55:40 +0200

    Add files via upload

---
+++ Kleine Geschmeus/Greeter Bot 1.1.py
@@ -0,0 +1,8 @@
+name = input("Was isch din Name? ").lower()
+if name == ("Sven") or ("Ranti"):
+    print ("Woah, de name tönt echt dumm!!! Haha ich bin froh heiss ich nöd so lol. " + name + " more like Svengay!!!")
+elif name ==("Dan") or ("dan") or ("Mort") or ("mort"):
+    print ("Hey" + name + ", du bisch cool für de 00:01 gebigruess, kuss auf d nuss")
+else:
+    print(name + " isch aber en schöne name")
+
